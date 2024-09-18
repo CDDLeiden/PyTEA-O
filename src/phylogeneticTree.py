5f1240a95c3e8948a5283bb244374ef75969118c
Sunah <bioin4matisean@gmail.com>
2024-09-18 12:52:50 -0500

    update multiprocessing

--- src/phylogeneticTree.py
+++ src/phylogeneticTree.py
@@ -1,6 +1,5 @@
 import numpy as np
 from datetime import datetime
-import os
 import sys
 
 multiprocessing = None
@@ -12,30 +11,86 @@ elif 'win' in sys.platform:
     multiprocessing = 'multiprocessing'
 mp = __import__(multiprocessing) #import multiprocessing as mp
 
+def dictreturn():
+    global seqDict
+    seqDict = {}
+    inputMSA = 'test_text.txt'
+    with open (inputMSA, 'r') as f:
+        for i, line in enumerate(f):
+            newLine = line.strip()
+            if newLine == '':
+                continue
+            if i >=3 and line[0]!=(' '): 
+                newLine = newLine.split()
+                if newLine[0] in seqDict.keys():
+                    seqDict[newLine[0]]+=newLine[1]
+                else:
+                    seqDict[newLine[0]] = newLine[1]
 
+    seqDict = dict(sorted(seqDict.items(), key=lambda x: x[0]))
+    return seqDict
 
-def phylo(inputMSA):
+def seqMreturn():
+    global seqMatrix
+    seqMatrix = np.zeros([len(seqDict),len(seqDict)])
 
-#make original matrix
-    # start_time = datetime.now()
+    start_time = datetime.now()
+    for i in range(0, len(seqMatrix)):
+        for j in range(0, len(seqMatrix)):
+            seqMatrix[i,j] = -1
+    # print("empty matrix done!")
+    end_time = datetime.now()
 
-    # for i, value1 in enumerate(seqDict.values()): #row
-    #     for j, value2 in enumerate(seqDict.values()): #column
-    #         if i<j: 
-    #             difference = 0
-    #             for k in range(len(value1)):
-    #                 if value1[k] != value2[k]:
-    #                     difference += 1
-                # seqMatrix[i, j] = difference
-        # print(f"{i}and {j} Done!")
+    print(end_time-start_time)
+    return seqMatrix
+    # print(seqDict)
 
-    # end_time = datetime.now()
-    # print(end_time-start_time)
+def differ(output):
+    # input()
 
+    row = [-1 for i in range(len(dictionary))]
+
+    for index, key in enumerate(dictionary.keys()):
+        if key == output:
+            i = index
+            print(output)
+            print(i)
+    # return(i)
+    # input()
     
+    for j, value2 in enumerate(dictionary.values()):
+        # print(value2)
+        if i<j :
+            difference = 0
+            for k in range(len(value2)):
+                # print(dictionary[output][k])
+                # input()
+                if value2[k] != dictionary[output][k]:
+                    difference += 1
+            # print("this is difference")
+            # print(difference)
+            # input()
+            # print("this is ij")
+            # print(i, j)
+            # print("this is  ij")
+             
+            row[j] = difference
+            
+            # print("this is matrix update")
+            # print(row)
 
 
-#make list of names
+            print(f"{i}and {j} Done!")
+    return row
+        
+def worker_init(seqdict, seqmatrix):
+    global dictionary
+    dictionary = seqdict
+    global matrix
+    matrix = seqmatrix
+    # print(dictionary)
+
+def grouping(seqDict, seqMatrix):
     key_list = []
     for i, keys in enumerate(seqDict.keys()):
         key_list.append(keys)
@@ -45,22 +100,12 @@ def phylo(inputMSA):
         location.append([i])
     
     #newick dictionary
-    newick = {}        
+    # newick = {}
 
-    # newick_format = key_list.copy()
-
-    '''
-    This is for tea-o
-    if you don't need it, you can delete it
-    '''
     tree = {
         0 : key_list.copy()
     }
-    '''
-    tea-o 
-    '''
 
-# make new matrix and update till len>2
     start_time = datetime.now()
     while(len(seqMatrix)>2):
         print(len(seqMatrix))
@@ -82,9 +127,9 @@ def phylo(inputMSA):
         key2 = key_list[column] # name in the (column)th value of the key list
         length = seqMatrix[row, column]/2
         
-        new_newick = "{},{}".format(key1, key2) #name of the key
-        newick[new_newick] = {key1: length,
-                              key2: length}    
+        # new_newick = "{},{}".format(key1, key2) #name of the key
+        # newick[new_newick] = {key1: length,
+        #                       key2: length}    
 
         new_key = "{},{}".format(key1, key2)
 
@@ -102,22 +147,6 @@ def phylo(inputMSA):
                 teao_list[i] = teao_list[i].split(',')
         # print(teao_list)
         tree[length] = teao_list
-        
-        # distance1 = (newick[new_newick])[key1]
-        # distance2 = (newick[new_newick])[key2]
-        # if key1 in newick.keys():
-        #     distance1 = (newick[new_newick])[key1] - [x for x in newick[key1].values()][0]
-
-        # if key2 in newick.keys():
-        #     distance2 = (newick[new_newick])[key2] - [x for x in newick[key2].values()][0]
-
-        # # input()
-
-        # new_new_newick = "({0}:{1},{2}:{3})".format(newick_format[row], distance1, newick_format[column], distance2)
-        # del newick_format[column]
-        # del newick_format[row]
-        # newick_format.insert(0, new_new_newick)
-        
 
 #make new array and update it
         arr2 = seqMatrix
@@ -157,90 +186,23 @@ def phylo(inputMSA):
     # final newick
     row = 0
     column = 1
-    # key1 = key_list[row]
-    # key2 = key_list[column]
-    
-    # if key1 in newick.keys():
-    #     final_dist1 = [x for x in newick[key1].values()][0]
-    # else:
-    #     final_dist1 = 0
-
-    # if key2 in newick.keys():
-    #     final_dist2 = [x for x in newick[key2].values()][0]
-    # else:
-    #     final_dist2 = 0
-
-    # final = "({}:{},{}:{});".format(newick_format[0], seqMatrix[row,column]/2 -final_dist1, newick_format[1] ,seqMatrix[row,column]/2 - final_dist2)
-
-
     tree[seqMatrix[row,column]/2] = [tree[0]]
 
-    with open("phylo3.tree", 'w') as file:
-        file.write(f"\t{"\t".join([f"{x}" for x in sorted(seqDict.keys())])}\n")
-        for i,row in enumerate(original_seqM):
-            for j,col in enumerate(row):
-                if i == j:
-                    # print("here")
-                    original_seqM[i,j] = 0
-            string = ""
-            for x in original_seqM[i]:
-                if x != -1:
-                    string += f"{x}\t"
-                else: 
-                    string += "\t"
-            file.write(f"{[f"{x}" for x in sorted(seqDict.keys())][i]}\t{string}\n")
-    
-    # with open("newick.txt", 'w') as file:
-    #     file.write(final)
-    
-    with open("test.txt", 'w') as file:
+    with open("grouping.txt", 'w') as file:
         for key, value in tree.items():
             file.write(f"{key}: {value}\n")
 
 
-def differ(arg):
-    print("this is differ")
-    result = arg
-    return result
-    
-seqDict = {}
-inputMSA = 'CS.clustal'
-with open (inputMSA, 'r') as f:
-    for i, line in enumerate(f):
-        newLine = line.strip()
-        if newLine == '':
-            continue
-        if i >=3 and line[0]!=(' '): 
-            newLine = newLine.split()
-            if newLine[0] in seqDict.keys():
-                seqDict[newLine[0]]+=newLine[1]
-            else:
-                seqDict[newLine[0]] = newLine[1]
-
-seqDict = dict(sorted(seqDict.items(), key=lambda x: x[0]))
-
-
-seqMatrix = np.zeros([len(seqDict),len(seqDict)])
-
-start_time = datetime.now()
-for i in range(0, len(seqMatrix)):
-    for j in range(0, len(seqMatrix)):
-        seqMatrix[i,j] = -1
-print("empty matrix done!")
-end_time = datetime.now()
-
-print(end_time-start_time)
-
 if __name__ == "__main__":
-    with mp.Pool(processes = 1) as executor:
-        inputs = list(seqDict.keys())
-        print("yes")
-        results = executor.map(differ, inputs)
+    seqDict = dictreturn()
+    seqMatrix = seqMreturn()
+    with mp.Pool(initializer= worker_init, initargs=(seqDict,seqMatrix), processes = mp.cpu_count()) as executor:
+        results = executor.map(differ, seqDict)
+    print("this is result")
+    # print(results)
+    # print(type(results))
+    matrix = np.array(results)
+    print(matrix)
+    # print(seqDict)
 
-        print(results)
-
-    input()
-
-
-
-phylo('CS.clustal')
+    grouping(seqDict, matrix)
