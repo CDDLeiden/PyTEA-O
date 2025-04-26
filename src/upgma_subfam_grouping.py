3740b999c0ccdb7420ff77453d0c1b5c43d1739c
Alexander <ajulian@hawk.iit.edu>
2025-04-25 20:00:16 -0500

    Converted UPGAM implementation to Pandas and Numpy. Fixed branch length subfamily grouping error.

--- src/upgma_subfam_grouping.py
+++ src/upgma_subfam_grouping.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-import os
+from os.path import isfile
+from os import makedirs
 import sys
 import argparse
 import numpy as np
 import pandas as pd
 from time import time
+from convert_alignment import read_MSA
+from warnings import simplefilter
+import gc
 
+simplefilter(action='ignore',category=pd.errors.PerformanceWarning)
 
 ## MacOS (darwin) utilizes a different multiprocessing library than Linux and Windows. Need to check which OS
 ## OS is running to prevent SNAFUs
@@ -18,48 +23,6 @@ elif 'darwin' in sys.platform:
 elif 'win' in sys.platform:
 	multiprocessing = 'multiprocessing'
 mp = __import__(multiprocessing)
-
-
-def read_MSA(MSA_File:str=None) -> dict:
-
-	"""Reads an MSA from a given file, and converts it to a dictionary object, with loci and sequences as keys and
-	values, repectively.
-
-	Inputs:
-		MSA_File: File path to MSA, either in FASTA or Clustal format.
-
-	Returns:
-		MSA: Dictionary object containig MSAs
-	"""
-
-	start = time()
-
-	msa = {}
-	with open (MSA_File, 'r') as f:
-
-		loci = None
-
-		for i, line in enumerate(f):
-
-			line = line.strip()
-			if line == '':
-				continue
-	
-			split_line = line.split()
-
-			if split_line[0][0] == ">":
-				loci = split_line[0][1:]
-				msa[loci] = []
-				continue
-			elif len(split_line) == 2:
-				loci = split_line[0]
-				line = split_line[1]
-
-			msa[loci].append(line)
-
-	msa = {x:set([(i,char) for i,char in enumerate("".join(msa[x]))]) for x in list(msa.keys())}
-
-	return msa
 
 def generate_distance_matrix(msa:dict) -> pd.DataFrame:
 
@@ -74,22 +37,23 @@ def generate_distance_matrix(msa:dict) -> pd.DataFrame:
 
 	keys:dict.keys = sorted(msa.keys())
 
-	return pd.DataFrame(np.full([len(msa),len(msa)],np.inf),columns=keys,index=keys)
+	return pd.DataFrame(np.zeros([len(msa),len(msa)]),columns=keys,index=keys)
 
 def distance(sequence_a_loc):
 
 	sequence_a = sequences[sequence_a_loc]
 
-	distances = {}
+	distances = {x:0 for x in sequences.keys()}
 
 	for sequence_b_loc in sorted(sequences.keys()):
 		if sequence_b_loc == sequence_a_loc:
+			distances[sequence_b_loc] = np.inf
 			break
-		distances[sequence_b_loc] = len(sequence_a.difference(sequences[sequence_b_loc]))
+		distances[sequence_b_loc] = sum(1 for a,b in zip(sequence_a,sequences[sequence_b_loc]) if a!=b)
 
 	return [sequence_a_loc,distances]
 
-def calculate_sequence_difference(msa:dict) -> pd.DataFrame:
+def calculate_sequence_difference(msa:dict,outfile:str) -> pd.DataFrame:
 
 	"""Calculates the distances between sequences in an MSA
 
@@ -97,168 +61,169 @@ def calculate_sequence_difference(msa:dict) -> pd.DataFrame:
 		pandas.Dataframe: NxN lower triangular matrix
 	"""
 
-	start = time()
+	if isfile(outfile):
+
+		print(f"\n\t\t[N] Loading previously calculated pairwise distances...\n")
+		return pd.read_csv(outfile,header=0,index_col=0)
 
 	distance_matrix = generate_distance_matrix(msa)
 
 	keys:dict.keys = sorted(msa.keys())
 	global sequences
 	sequences = msa
+	
+	start = time()
 
+	results:list
 	with mp.Pool(mp.cpu_count()) as executor:
 		results = executor.map(distance,keys)
 
-	print(f"Calculated {len(keys)**2/2} distances in {int(time()-start)}s")
+	print(f"\n\t\tDistance calculations took {int(time()-start)}s\n")
 
+	pause = time()
+	item:int = 0
+	for sequence_loc_a,result in results:
+		item += 1
+		distance_matrix.loc[sequence_loc_a] = result
+	print(f"\n\t\tDistance assignment took {int(time()-pause)}s\n")
 
-	exit()
+	print(f"\n\t\tGenerated {len(keys)}x{len(keys)} distance matrix in {int(time()-start)}s\n")
+
+	distance_matrix.to_csv(outfile)
+
 	return distance_matrix
 
-def grouping(msa:dict, outfile:str):
+def UPGMA(distance_matrix:pd.DataFrame) -> dict:
 
-	distance_matrix:pd.DataFrame = calculate_sequence_difference(msa)
-	
-	keys = list(msa.keys())
-
-	original_distance_matrix = distance_matrix.copy()
-	# location = []
-	# for i in range(0,len(distance_matrix)):
-	# 	location.append([i])
-
-	# print(location)
-
-	# with open(outfile, "w") as file:
-	# 	first_line = "## " + ";".join(keys) + "\n"
-	# 	file.write(first_line)
-
-	tree = {
-		0 : keys
-	}
-
-	# start_time = datetime.now()
-	while(len(distance_matrix)>2):
-		# row = 0
-		# m = len(seqMatrix)
-		flattend_index =  np.argmin(distance_matrix)
-
-		row = keys[flattend_index//distance_matrix.shape[1]]
-		col = keys[flattend_index%distance_matrix.shape[1]]
-
-		print(row,col,distance_matrix.loc[row,col])
-
-		exit()
-		# while(index-(m-row-1)>=0): 
-		# 	index -= (m-row)-1
-		# 	row += 1
-		# column = 1
-		# while(index-1>=0):
-		# 	index -= 1
-		# 	column += 1
-		# column += row
-	
-		#make newick format
-		key1 = keys[row] # Name in the (row)th value of key list
-		key2 = keys[column] # Name in the (column)th value of the key list
-		length = seqMatrix[row, column]/2
-		
-		new_key = "{},{}".format(key1, key2)
-
-		del keys[column]
-		del keys[row]
-
-		keys.insert(0,new_key)
-		'''
-		this is for teao
-		'''
-		teao_list = keys.copy()
-		for i in range(len(teao_list)):
-			if ',' in teao_list[i]:
-				teao_list[i] = teao_list[i].split(',')
-		tree[length] = teao_list
-
-		# Make new array and update it
-		arr2 = seqMatrix
-		arr2 = np.delete(arr2, column, axis = 0)
-		arr2 = np.delete(arr2, column, axis = 1)
-
-		arr2 = np.delete(arr2, row, axis = 0)
-		arr2 = np.delete(arr2, row, axis = 1)
-		
-		arr2 = np.insert(arr2, 0, -1, axis = 1) # (arr, obj, values, axis)
-		arr2 = np.insert(arr2, 0, -1, axis = 0)
-
-
-		new_name = location[row]+location[column]
-		location.insert(0,new_name)
-
-		del location[column+1]
-		del location[row+1]
-
-		for l in range(1, len(arr2)):
-			sum1 = 0
-			for m in range(0, len(location[0])): # [abc]
-				for k in range(0, len(location[l])): # [de],[f],[g]
-					if (location[0])[m] > (location[l])[k]:
-						sum1 += original_seqM[(location[l])[k], (location[0])[m]]
-					else:
-						sum1 += original_seqM[(location[0])[m],(location[l])[k]]
-					arr2[0,l] = sum1/((len(location[0]))*(len(location[l])))
-
-		seqMatrix = arr2
-	end_time = datetime.now()
-	print(end_time-start_time)
-	
-	# Final newick
-	row = 0
-	column = 1
-	tree[seqMatrix[row,column]/2] = [tree[0]]
-
-	# level = 0
-	tree_red = {}
-	tree_content = {}
-
-	for j, value in enumerate(tree.values()):
-		for i in range(len(value)):
-			if type(value[i])==list:
-				# print(value[i])
-				for k in range(len(value[i])):
-					# print(value[i][k])
-					if type(value[i][k])==str:
-						tree_content[value[i][k]] = hex(i)
-					elif type(value[i][k])==list:  # Duplicate sequences
-						print(value[i][k])
-						sys.exit()
-						tree_content[str(value[i][k])] = hex(i)
-			else:
-				# print(value[i])
-				tree_content[value[i]] = hex(i)
-		content = tree_content.copy()
-		tree_red[j] = content
-		grouping_list = list(content.values())
-		grouping_line = ";".join(grouping_list)
-		with open(outfile, 'a') as file:
-			file.write(grouping_line+"\n")
-
-def main(args):
+	print(f"\n\t\tStarting UPGMA tree building")
 
 	start = time()
 
-	msa = read_MSA(MSA_File=args.msa)
+	distance_matrix = distance_matrix + distance_matrix.T
 
-	# Create output filename
-	basename = os.path.splitext(args.msa)[0]
-	outname = os.path.join(basename + "_grouping.txt")
-	grouping(msa,outname)
-	exit()
+	distances:dict = {x:0 for x in distance_matrix.index}
 
+	tree:dict = {0:distance_matrix.columns.to_list()}
 
+	while(len(distance_matrix)>2):
+
+		flattend_index =  np.argmin(distance_matrix)
+
+		row_i = flattend_index//distance_matrix.shape[1]
+		col_i = flattend_index%distance_matrix.shape[1]
+
+		row = distance_matrix.columns[row_i]
+		col = distance_matrix.columns[col_i]
+
+		pairwise_dist = distance_matrix.loc[row,col]
+
+		data_A = np.delete(distance_matrix.loc[row].to_numpy(),[row_i,col_i])
+		weight_A = len(row.split(","))
+		data_B = np.delete(distance_matrix.loc[col].to_numpy(),[row_i,col_i])
+		weight_B = len(col.split(","))
+
+		new_data = np.append((((data_A*weight_A) + (data_B*weight_B))/(weight_A+weight_B)),np.inf)
+
+		distance_matrix = distance_matrix.drop(index=[row,col],columns=[row,col])
+
+		joined_name = f"{row},{col}"
+
+		tree[pairwise_dist/2] = distance_matrix.columns.to_list() + [joined_name]
+
+		distance_matrix[joined_name] = new_data[:-1]
+		distance_matrix.loc[joined_name] = new_data
+
+		gc.collect()
+
+	row,col = distance_matrix.columns.to_numpy()
+
+	tree[distance_matrix.loc[row,col]] = [",".join(tree[0])]
+	
+	print(f"\n\t\t\tCondensed {len(tree[0])} sequences in {int(time()-start)}s\n")
+
+	return tree
+
+def generate_subgroups(tree:dict,outfile:str)-> None:
+	
+	subfamilies = {}
+	for branchpoint, distance in enumerate(sorted(tree.keys())):
+		subfamilies[branchpoint] = {}
+		for group_num,group in enumerate(sorted(tree[distance])):
+			for member in group.split(","):
+				subfamilies[branchpoint][member] = hex(group_num)
+
+	num_of_groups = np.inf
+	with open(outfile, "w") as file:
+		headers = ";".join(tree[0])
+		file.write(f"{headers}\n")
+		for branch in subfamilies:
+			grouping_line = ";".join([subfamilies[branch][member] for member in sorted(subfamilies[branch].keys())])
+			file.write(f"{grouping_line}\n")
+			num = len(np.unique([subfamilies[branch][member] for member in sorted(subfamilies[branch].keys())]))
+			if num > num_of_groups: print("OH NO!!!")
+			num_of_groups = num
+
+	return None
+
+def pass_subfamilies_file_check(outfile:str,labels:list) -> bool:
+
+	if not isfile(outfile):
+
+		return False
+
+	subfamilies_data:pd.DataFrame
+
+	try:
+
+		subfamilies_data = pd.read_csv(outfile,header=0,index_col=0,delimiter=";")
+
+	except pd.errors.EmptyDataError as error:
+
+		print(f"\n\t\t[N] {outfile} exists, but contains no subfamily information.\n")
+
+		return False
+
+	if (labels != sorted(subfamilies_data.columns.to_list())) and (np.unique(subfamilies_data.iloc[-1].to_numpy()) != ['0x0']):
+
+		print(f"\n\t\t[N] {outfile} exists, but subfamily information is incomplete.\n")
+
+		return False
+
+	print(f"\n\t\t[N] {outfile} exists, and subfamily information is complete.\n")
+	print(f"\n\t\t[N] Terminating UPGMA grouping. Use -f|--force to force algorithm run.\n")
+
+	return True
+
+def run(msa_file:str,outdir:str) -> None:
+
+	start = time()
+
+	makedirs(outdir,0o700,exist_ok=True)
+
+	## Read in MSA
+	file_prefix,msa = read_MSA(msa_file=msa_file)
+
+	## Create and fill initial distance matrix
+	distance_matrix:pd.DataFrame = calculate_sequence_difference(msa=msa,outfile=f"{outdir}/{file_prefix}.dist")
+
+	subfamilies_file = f"{outdir}/{file_prefix}.subfamilies"
+
+	## No re-runs
+	if pass_subfamilies_file_check(outfile=subfamilies_file,labels=sorted(msa.keys())): return None
+
+	## Perform UPGMA tree analysis
+	tree = UPGMA(distance_matrix=distance_matrix)
+
+	## Identify subgroups from UPGMA collapse pattern
+	generate_subgroups(tree=tree,outfile=subfamilies_file)
+
+	return None
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description="""Grouping subfamilies based on UPGMA clustering. Input is a Multiple Sequence Alignment (CLUSTAL format)
-													and the output is a text file containing the subfamily grouping. The output file is named as 
-													<input_filename>_grouping.txt""")
-	parser.add_argument("-m", "--msa", required=True, help="file location of Multiple Sequence Alignment (CLUSTAL format)")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-m", "--msa_file", required=True)
+	parser.add_argument("-o","--outdir",default="UPGMA_SUBFAMILY_GROUPINGS")
 	args = parser.parse_args()
 
-	main(args)
+	run(msa_file=args.msa_file,outdir=args.outdir)
