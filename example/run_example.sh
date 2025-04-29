c4c658a05827b561566ba80d283239e903509c05
rcmkuin <r.c.m.kuin@lacdr.leidenuniv.nl>
2025-04-29 15:29:54 +0200

    update example

--- example/run_example.sh
+++ example/run_example.sh
@@ -3,47 +3,24 @@
 SCRIPT_DIR="$(dirname "$0")"
 codebasedir="${SCRIPT_DIR}/../src"
 configfile="${SCRIPT_DIR}/../config/graph_results.cfg"
-msafile="${SCRIPT_DIR}/example_MSA.clustal"
+msafile="${SCRIPT_DIR}/example.fasta"
 outputdir="${SCRIPT_DIR}/results"
 refid="0_0_0"
 
 # Create output directory
 mkdir -p ${outputdir}
 
-# Create one-liner alignment
-echo "Create one-liner .clustal alignment"
-msafolder=$(dirname "${msafile}")
-perl ${codebasedir}/make_aln_oneliner.pl \
-    -i ${msafile} \
-    -o ${msafolder}
-
 # Create subgroups
 echo "Make subgroups"
 python ${codebasedir}/upgma_subfam_grouping.py \
-    -m ${msafile}
+    -m ${msafile} \
+    -o ${outputdir}
 
 # Run TEA-O calculations
 echo "Run TEA calculations"
-python ${codebasedir}/shannon_entropy.py \
-    -m "example_MSA/example_MSA.olaln" \
+python ${codebasedir}/run_TEA.py \
+    -m ${msafile} \
     -o ${outputdir} \
-    -p "TEAO" \
     -r ${refid} \
-    -s "example_MSA_grouping.txt" \
-    -t 12
-
-# Calculate z-scales
-echo "Calculate z-scales"
-python ${codebasedir}/z_scales.py \
-    -m "example_MSA/example_MSA.olaln" \
-    -o ${outputdir}
-
-# plot the results
-echo "Plot the results"
-python ${codebasedir}/graph_results.py \
-    -s ${outputdir}/${refid}.shannon_entropy.txt \
-    -c ${outputdir}/consensus_logo.txt \
-    -a ${outputdir}/teao_average_shannon_entropy.txt \
-    -z ${outputdir}/zscales.txt \
-    -o ${outputdir} \
-    -l ${configfile}
+    -t 12 \
+    -f ${outputdir}/example_MSA.subfamilies
