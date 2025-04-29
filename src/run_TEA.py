79aa6a07b87aba0768fd03b94205ef177784d373
Alexander <ajulian@hawk.iit.edu>
2025-04-29 00:39:22 -0500

    Removed unnecessary one-line conversion step. Moved UPGMA call to withing Shannon Entropy file as only needed for TEAO

--- src/run_TEA.py
+++ src/run_TEA.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from convert_alignment import run as convert_alignment
-from upgma_subfam_grouping import run as upgma_subfam_grouping
 from shannon_entropy import run as shannon_entropy
 from z_scales import run as z_scales
 from graph_results import run as graph_results
@@ -11,26 +9,10 @@ def run(args:ArgumentParser.parse_known_args) -> None:
 
 	file_prefix = args.msa_file.split("/")[-1].split(".")[0]
 
-
-	print("\n\n\tConverting MSA into compatible one-liner format.")
-	convert_alignment(
-		msa_file=args.msa_file,
-		outdir=args.outdir,
-		reference=args.reference,
-		desired_alignment_type='one_liner',
-		extract_fastas=True
-	)
-
-	print("\n\n\tGenerating UPGMA-based subfamily groupings.")
-	upgma_subfam_grouping(
-		msa_file=args.msa_file,
-		outdir=args.outdir
-	)
-
 	print("\n\n\tPerforming Two-Entropy Analysis.")
 	shannon_entropy(
-		path_msa=args.msa_file,
-		subfamilies_file=f"{args.outdir}/{file_prefix}.subfamilies",
+		msa_file=args.msa_file,
+		subfamilies_file=args.subfamily_file,
 		outdir=args.outdir,
 		mode=args.tea_mode,
 		reference_id=args.reference,
@@ -46,14 +28,12 @@ def run(args:ArgumentParser.parse_known_args) -> None:
 
 	print("\n\n\tGraphing TEA-O results.")
 	graph_results(
-		shannon_entropy_file=f"{args.outdir}/{args.reference}.shannon_entropy.txt",
+		shannon_entropy_summary_file=f"{args.outdir}/shannon_entropy.summary",
 		outdir=f"{args.outdir}",
-		highlight_file=args.highlight_file,
-		concensus_sequence_file=f"{args.outdir}/consensus_logo.txt",
-		average_entropy_file=f"{args.outdir}/teao_average_shannon_entropy.txt",
+		highlight_residue_file=args.highlight_file,
 		subset_file=args.subset_file,
-		zscale_file=f"{args.outdir}/zscales.txt",
-		configuration=args.configuration
+		zscale_file=f"{args.outdir}/zscales.tsv",
+		configuration_file=args.configuration
 	)
 
 
@@ -72,8 +52,9 @@ if __name__ == "__main__":
 	GetOptions.add_argument("-e","--tea_mode",default="TEAO",choices=["TEA","TEAO"])
 	GetOptions.add_argument("-t","--threads",type=int,default=1)
 	GetOptions.add_argument("-d","--descriptors",default='Zscale Sandberg')
-	GetOptions.add_argument("-l","--highlight_file")
-	GetOptions.add_argument("-s","--subset_file")
+	GetOptions.add_argument("-l","--highlight_file",default=None,type=str)
+	GetOptions.add_argument("-f","--subfamily_file",default=None,type=str)
+	GetOptions.add_argument("-s","--subset_file",default=None,type=str)
 	GetOptions.add_argument("-c","--configuration",default=f"{dirname(abspath(__file__))}/../config/graph_results.cfg")
 
 	run(GetOptions.parse_known_args()[0])
