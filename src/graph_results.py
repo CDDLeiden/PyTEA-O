c94dc504cb119c21b42a87629eba434dfb75d67c
Alexander <ajulian@hawk.iit.edu>
2025-06-13 13:18:35 -0500

    Reworked residue highlighting and and plot selection.

--- src/graph_results.py
+++ src/graph_results.py
@@ -204,82 +204,69 @@ def plot(data:dict=None,
 
 	fig = plt.figure(figsize=(width,plot_rows*2))
 
-	current_row = 0
-	final_plot = None
-	first_plot = None
+	plots = []
 
 	## Specifying plot placements
 	# Shannon Entropy 1st
 	if config["se_graphing"]:
-		se_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0))
-		if current_row == 0:
-			first_plot = se_graph
-		current_row += 1
-		final_plot = se_graph
+		se_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0))
+		plots.append(se_graph)
+	
 	# Specificty/Conservation Scoring
 	if config["specificity_graphing"]:
-		scs_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0))
-		if current_row == 0:
-			first_plot = scs_graph
-		current_row += 1
-		final_plot = scs_graph
+		scs_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0))
+		plots.append(scs_graph)
+	
 	if config["residue_presence_matrix"]:
-		res_mat_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0),rowspan=2)
-		if current_row == 0:
-			first_plot = res_mat_graph
-		current_row += 2
-		final_plot = res_mat_graph
+		res_mat_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0),rowspan=2)
+		plots.append(res_mat_graph)
+		plots.append(res_mat_graph)
+
 	# Number of different residues present at site
 	if config["number_of_residues"]:
-		res_num_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0))
-		if current_row == 0:
-			first_plot = res_num_graph
-		current_row += 1
-		final_plot = res_num_graph
+		res_num_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0))
+		plots.append(res_num_graph)
+
 	# Number of sequences belonging to that residue
 	if config["residue_distribution"]:
-		res_dist_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0))
-		if current_row == 0:
-			first_plot = res_dist_graph
-		current_row += 1
-		final_plot = res_dist_graph
+		res_dist_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0))
+		plots.append(res_dist_graph)
+
 	# protein descriptor distribution
 	if config["descriptor_distribution"]:
-		descriptor_graph = plt.subplot2grid((plot_rows,plot_columns),(current_row,0))
-		if current_row == 0:
-			first_plot = descriptor_graph
-		current_row += 1
-		final_plot = descriptor_graph
+		descriptor_graph = plt.subplot2grid((plot_rows,plot_columns),(len(plots),0))
+		plots.append(descriptor_graph)
 
 	colors = {
-		"white":"ffffffff",
-		"maroon":"800000",
-		"brown":"9A6324",
-		"olive":"808000",
-		"teal":"469990",
-		"navy":"000075",
-		"red":"e6194B",
-		"orange":"f58231",
-		"yellow":"ffe119",
-		"lime":"bfef45",
-		"green":"3cb44b",
-		"cyan":"42d4f4",
-		"blue":"4363d8",
-		"purple":"911eb4",
-		"magenta":"f032e6",
-		"grey":"a9a9a9",
-		"pink":"fabed4",
-		"apricot":"ffd8b1",
-		"beige":"fffac8",
-		"mint":"aaffc3",
-		"lavender":"dcbeff",
+		"white":"#ffffffff",
+		"maroon":"#800000",
+		"brown":"#9A6324",
+		"olive":"#808000",
+		"teal":"#469990",
+		"navy":"#000075",
+		"red":"#e6194B",
+		"orange":"#f58231",
+		"yellow":"#ffe119",
+		"lime":"#bfef45",
+		"green":"#3cb44b",
+		"cyan":"#42d4f4",
+		"blue":"#4363d8",
+		"purple":"#911eb4",
+		"magenta":"#f032e6",
+		"grey":"#a9a9a9",
+		"pink":"#fabed4",
+		"apricot":"#ffd8b1",
+		"beige":"#fffac8",
+		"mint":"#aaffc3",
+		"lavender":"#dcbeff",
 	}
 
 	color_keys = colors.keys()
 
-	custom_colors = [f"#{x}" for x in colors.values()]
+	custom_colors = [x for x in colors.values()]
+	highlight_colors = [colors[x] for x in colors.keys() if x in ['maroon','brown','olive','teal','navy','red','orange','green','blue','purple','magenta']]
 
-	cmap = mcolors.ListedColormap([f"#{x}" for x in colors.values()])
+	cmap = mcolors.ListedColormap([x for x in colors.values()])
 
 	####################################################################################################################
 	## Shannon Entropy Graphing
@@ -290,7 +277,7 @@ def plot(data:dict=None,
 	ase_values = np.array([data[x]["ASE"] for x in res_nums])
 
 	# Residue and residue number labels
-	labels = [f"{data[x]['RES']} [{x+1}]" if x%2 != 0 else f"{data[x]['RES']}" for x in res_nums]
+	labels = [x if x%2 != 0 else "" for x in res_nums]
 
 	if config["se_graphing"]:
 		## Expand the graph slightly past the plotted data
@@ -305,13 +292,16 @@ def plot(data:dict=None,
 		# Green (0.0-0.25)
 		# Yellow (0.25-0.75)
 		# Red (0.75-1.0)
-		se_graph.add_patch(plt.Rectangle((res_nums[0]-1,-0.05),(res_nums[-1]+2),0.3,color='g',alpha=0.1))
-		se_graph.add_patch(plt.Rectangle((res_nums[0]-1,.25),(res_nums[-1]+2),0.5,color='y',alpha=0.1))
-		se_graph.add_patch(plt.Rectangle((res_nums[0]-1,.75),(res_nums[-1]+2),1,color='r',alpha=0.1))
+		# se_graph.add_patch(plt.Rectangle((res_nums[0]-1,-0.05),(res_nums[-1]+2),0.3,color='g',alpha=0.1))
+		# se_graph.add_patch(plt.Rectangle((res_nums[0]-1,.25),(res_nums[-1]+2),0.5,color='y',alpha=0.1))
+		# se_graph.add_patch(plt.Rectangle((res_nums[0]-1,.75),(res_nums[-1]+2),1,color='r',alpha=0.1))
+		se_graph.plot(res_nums,[0.25 for x in res_nums],color='r',alpha=0.25)
+		se_graph.plot(res_nums,[0.5 for x in res_nums],color='y',alpha=0.25)
+		se_graph.plot(res_nums,[0.75 for x in res_nums],color='g',alpha=0.25)
 
 		## Plot the data
-		se_graph.plot(res_nums,se_values,linewidth=0.5,color='k')
-		se_graph.plot(res_nums,ase_values,linewidth=0.5,color='b')
+		se_graph.plot(res_nums,se_values,linewidth=0.5,color=colors['maroon'],linestyle="-",marker='.',label='Global Entropy')
+		se_graph.plot(res_nums,ase_values,linewidth=0.5,color=colors['cyan'],linestyle="-",marker='.',label='Average Entropy')
 
 		## Prettify the Y-axis
 		se_graph.yaxis.set_ticks([i*0.2 for i in range(int(1/0.2)+1)])
@@ -321,46 +311,37 @@ def plot(data:dict=None,
 		se_graph.xaxis.grid(color='w',linestyle='-',linewidth=0.75,which='minor')
 		se_graph.spines['bottom'].set_visible(False)
 
+		se_graph.vlines([x+0.5 for x in range(len(res_nums)-1)],colors="#f9f9f9ff",ymin=0,ymax=1)
+
+		se_graph.legend(loc='center right',bbox_to_anchor=(-0.01,0.5))
+
 	####################################################################################################################
 	## Specificity/Conservation Scoring
 	####################################################################################################################
 
-	rotate_deg = np.deg2rad(45)
-	sin_val = np.sin(rotate_deg)
-	cos_val = np.cos(rotate_deg)
-	off = 0.5
-	h = np.sqrt(np.square(ase_values)+np.square(se_values))
-	cons = np.zeros(h.shape)
-	np.seterr(divide='ignore',invalid='ignore')
-	cons = -h*np.cos(np.arccos(ase_values/h)-rotate_deg)
-	np.nan_to_num(cons,copy=False)
-	cons += off
-	spec_dist = np.sqrt(np.square(ase_values)+np.square(np.subtract(se_values,1)))
-
-	for index,x in enumerate(res_nums):
-		start_y = cons[index] if cons[index] < 0 else 0
-		length_y = abs(cons[index])
-		rect_width = 1-spec_dist[index]
-		rect_start = x - (0.5*rect_width)
-		if config["specificity_graphing"]:
-			scs_graph.add_patch(plt.Rectangle((rect_start,start_y),rect_width,length_y,facecolor='b',edgecolor='b'))
-
-	###########################################################
-	## Hightlight user-provided residues
-	###########################################################
-	if highlights is not None:
-		rand_colors = sample(range(len(color_keys)),len(highlights.keys()))
-		for index,source in enumerate(highlights.keys()):
-			for val_x in highlights[source]:
-				if config["specificity_graphing"]:
-					scs_graph.add_patch(plt.Rectangle((val_x-0.5,-0.6),1,1.2,facecolor=colors[f'xkcd:{color_keys[rand_colors[index]]}'],edgecolor="None",zorder=0,alpha=0.25))
-	
 	if config["specificity_graphing"]:
+
+		conserved_scores = -np.sqrt(np.square(ase_values)+np.square(se_values))+0.5
+		specificity_scores = -np.sqrt(np.square(ase_values)+np.square(se_values-1))+0.5
+
+		scs_graph.plot([i for i in range(len(conserved_scores))],conserved_scores,color=colors['teal'],linewidth=0.5,marker=".",label='Residue Conservation')
+		scs_graph.plot([i for i in range(len(conserved_scores))],specificity_scores,color=colors['purple'],linewidth=0.5,marker=".",label='Residue Specificity')
+		scs_graph.legend(loc='center right',bbox_to_anchor=(-0.01,0.5))
+		scs_graph.plot([x+0.5 for x in range(len(res_nums))],[0 for i in res_nums],color="#000000aa",zorder=0)
+
+		# for index,x in enumerate(res_nums):
+		# 	start_y = cons[index] if cons[index] < 0 else 0
+		# 	length_y = abs(cons[index])
+		# 	rect_width = 1-spec_dist[index]
+		# 	rect_start = x - (0.5*rect_width)
+		# 	if config["specificity_graphing"]:
+				# scs_graph.add_patch(plt.Rectangle((rect_start,start_y),rect_width,length_y,facecolor='b',edgecolor='b'))
+				# zorder = 1_000_000 if abs(cons[index]) < abs(spec_dist[index]) else 0
+				# scs_graph.bar(index,cons[index],edgecolor='b',facecolor="none",zorder=zorder)
+				# scs_graph.bar(index,spec_dist[index],edgecolor='k',facecolor="none")
+	
 		# Set and rotate major tick labels
-		if scs_graph == first_plot:
-			labels = [None for val_x in res_nums]
-		else:
-			labels = [f"{val_x+1: >{len(str(len_of_seq))+1}}" if val_x%2 != 0 else "" for val_x in res_nums]
+
 		scs_graph.xaxis.set_ticks([i for i in res_nums])
 		scs_graph.tick_params(axis='x',which='both',bottom=False,labelbottom=False,top=False,labeltop=False)
 		scs_graph.set_xticklabels(labels=labels,rotation='vertical',font='monospace',fontsize=font_size,va='center',ha='center')
@@ -374,17 +355,20 @@ def plot(data:dict=None,
 		scs_graph.set_xlim([(res_nums[0]-0.5),res_nums[-1]+0.5])
 		scs_graph.set_ylim([-0.65,0.65])
 
+		scs_graph.vlines([x+0.5 for x in range(len(res_nums)-1)],colors="#f9f9f9ff",ymin=-.75,ymax=.75)
+
 	####################################################################################################################
 	## Residue Presence Matrix
 	####################################################################################################################
 
-	## Get a positive/negative matrix of what residues are present in the MSA at each residue
-	present_res = np.zeros((len(aas),len(res_nums)))
-	for row_index,aa in enumerate(aas):
-		for col_index,val_x in enumerate(res_nums):
-			present_res[row_index][col_index] = row_index+1 if data[val_x]["RES_COUNTS"][aa] > 0 else 0
-
 	if config["residue_presence_matrix"]:
+
+		## Get a positive/negative matrix of what residues are present in the MSA at each residue
+		present_res = np.zeros((len(aas),len(res_nums)))
+		for row_index,aa in enumerate(aas):
+			for col_index,val_x in enumerate(res_nums):
+				present_res[row_index][col_index] = row_index+1 if data[val_x]["RES_COUNTS"][aa] > 0 else 0
+
 		res_mat_graph.matshow(present_res,aspect='auto',cmap=cmap)
 
 		## Setup Y-Axis
@@ -399,34 +383,28 @@ def plot(data:dict=None,
 		res_mat_graph.set_xticklabels(labels=labels,rotation='vertical',font='monospace',fontsize=font_size,va='center',ha='center')
 		# Show major tick labels, but not the ticks themselves, and only on the top
 		res_mat_graph.tick_params(axis='x',which='major',top=False,bottom=False,labeltop=True,labelbottom=False)
-
-		# Set minor ticks for each residue, but offset by -0.5 to create fake gridlines
-		res_mat_graph.xaxis.set_minor_locator(MultipleLocator(1))
-		# Don't show any information regarding the minor ticks
-		res_mat_graph.tick_params(axis='x',which='minor',top=False,bottom=False,labeltop=False,labelbottom=False)
 		
 		# Hide the top and bottom axes
 		res_mat_graph.spines['top'].set_visible(False)
 		res_mat_graph.spines['bottom'].set_visible(False)
 
-		# Turn on X-axis gridlines using the minor ticks
-		res_mat_graph.xaxis.grid(color='w',linestyle='-',linewidth=0.75,which='minor')
+		res_mat_graph.vlines([x+0.5 for x in range(len(res_nums)-1)],colors='#f9f9f9ff',ymin=-0.5,ymax=len(aas)-0.5)
+
+		res_mat_graph.set_zorder(1_000_000)
+		
 
 	####################################################################################################################
 	## Number of Residue Bar Graph
 	####################################################################################################################
 
 	if config["number_of_residues"]:
-		res_num_graph.bar(res_nums,num_ress,1)
+
+		res_num_graph.bar(res_nums,num_ress,1,color=colors['navy'])
 
 		## Setup X-Axis
 		# Set major tick for each residue [0 to pos-1]
 		res_num_graph.xaxis.set_ticks([i for i in res_nums])
 		# Set and rotate major tick labels
-		if res_num_graph == first_plot:
-			labels = [None for val_x in res_nums]
-		else:
-			labels = [f"{val_x+1: >{len(str(len_of_seq))+1}}" if val_x%2 != 0 else "" for val_x in res_nums]
 		res_num_graph.set_xticklabels(labels=labels,rotation='vertical',font='monospace',fontsize=font_size,va='center',ha='center')
 		res_num_graph.tick_params(axis='x',which='major',top=False,bottom=False,labelbottom=False,labeltop=True)
 
@@ -442,13 +420,18 @@ def plot(data:dict=None,
 		
 		# Show horizontal guidelines
 		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[5,5],color='w',linestyle='--',linewidth=1)
-		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[10,10],color='aqua',linestyle='-',linewidth=1)
+		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[10,10],color=colors['green'],linestyle='-',linewidth=1)
 		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[15,15],color='w',linestyle='--',linewidth=1)
-		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[20,20],color='aqua',linestyle='-',linewidth=1)
+		res_num_graph.plot([res_nums[0]-0.5,res_nums[-1]+0.5],[20,20],color=colors['green'],linestyle='-',linewidth=1)
+		res_num_graph.set_ylabel("# of Unique Residues")
 
 		# Hide the top and bottom axes
 		res_num_graph.spines['top'].set_visible(False)
 		res_num_graph.spines['bottom'].set_visible(False)
+
+		res_num_graph.vlines([x+0.5 for x in range(len(res_nums)-1)],colors='#f9f9f9ff',ymin=0,ymax=len(aas))
+
+		res_num_graph.set_zorder(1_000_000)
 	
 	###################################################################################################################
 	## Residue Distribution Bar Graph
@@ -457,9 +440,10 @@ def plot(data:dict=None,
 	# TODO Add colorscheme here for residue classification (pos, neg, etc.)
 	# Stacking bars require a y-value to place the bottom of the bar
 	
-	bottoms = np.zeros(len_of_seq)
-
 	if config["residue_distribution"]:
+
+		bottoms = np.zeros(len_of_seq)
+
 		# Iterate over all amino acids
 		for i,aa in enumerate(aas[::-1]):
 			res_vals = np.zeros(len_of_seq)
@@ -487,6 +471,10 @@ def plot(data:dict=None,
 
 		res_dist_graph.spines['top'].set_visible(False)
 		res_dist_graph.spines['bottom'].set_visible(False)
+
+		res_dist_graph.set_ylabel("Residue Distribution")
+
+		res_dist_graph.set_zorder(1_000_000)
 
 	###################################################################################################################
 	## ZSCALES
@@ -525,26 +513,42 @@ def plot(data:dict=None,
 		descriptor_graph.spines['bottom'].set_visible(False)
 		descriptor_graph.spines['top'].set_visible(False)
 
+		xmin,ymin,width,height = descriptor_graph.get_position(original=True).bounds
+
+		descriptor_graph.set_zorder(1_000_000)
+
 	## Setup top and bottom X-Axis
-	axt = first_plot.secondary_xaxis('top')
+	axt = plots[0].secondary_xaxis('top')
 	axt.set_xticks(range(len(res_nums)))
 	axt.tick_params(which='both',top=False)
 	labels = [f"{data[x]['RES']} [{x+1}]" if x%2 != 0 else f"{data[x]['RES']}" for x in res_nums]
 	axt.set_xticklabels(labels,font='monospace',fontsize=font_size,ha='center',va='bottom',rotation='vertical')
 
-	rdg_bt = final_plot.secondary_xaxis("bottom")
+	rdg_bt = plots[-1].secondary_xaxis("bottom")
 	rdg_bt.set_xticks(range(len(res_nums)))
 	rdg_bt.tick_params(which='both',bottom=False)
 	labels = [f"[{x+1}] {data[x]['CON_RES']}" if x%2 != 0 else data[x]['CON_RES'] for x in res_nums]
 	rdg_bt.set_xticklabels(labels=labels,rotation='vertical',font='monospace',fontsize=font_size)
 
+	plt.tight_layout()
+
+	###########################################################
+	## Hightlight user-provided residues
+	###########################################################
+	if highlights is not None:
+		rand_colors = sample(range(0,len(highlight_colors)-1),len(highlights.keys()))
+		xmin,ymax,width,height = plots[0].get_position(original=True).bounds
+		_,ymin,_,_ = plots[-1].get_position(original=True).bounds
+		step = width/(len(res_nums))
+		for index,source in enumerate(highlights.keys()):
+			for val_x in highlights[source]:
+				fig.patches.append(plt.Rectangle((xmin+((val_x+0.25)*step),0),step/2,1,facecolor=highlight_colors[rand_colors[index]],edgecolor="None",zorder=0,alpha=0.25,transform=fig.transFigure))
+
 	###################################################################################################################
 	## SAVING FILES
 	###################################################################################################################
 
-	fig.canvas.draw_idle()
-	plt.tight_layout()
-	plt.savefig(fname_out)
+	plt.savefig(fname_out,dpi=300)
 	
 	return fig
 
