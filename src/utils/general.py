319a52c68f330e0b7c0bcf15553f1e75de00dc79
Alexander <ajulian@hawk.iit.edu>
2025-09-12 10:25:06 -0500

    Directory validation and required arugment parsing added

--- src/utils/general.py
+++ src/utils/general.py
@@ -5,9 +5,35 @@ import argparse
 import inspect
 
 def valid_file(path:str) -> pathlib.Path:
-	p = pathlib.Path(path)
+
+	p = pathlib.Path(path).resolve(strict=False)
+	
 	if not p.is_file():
 		raise argparse.ArgumentTypeError(f"File not found: {path}")
+	
+	return p
+
+def valid_directory(path:str) -> pathlib.Path:
+
+	p = pathlib.Path(path).resolve(strict=False)
+
+	try:
+	
+		## Make the directory and necessary parent directories
+		p.mkdir(mode=0o755,parents=True,exist_ok=True)
+
+		## Make sure directory is writable
+		test_file = p/'.write_test'
+		with open(test_file,'w') as f:
+			f.write('test')
+
+		## Remove test file
+		test_file.unlink()
+
+	except Exception as e:
+
+		raise PermissionError(f"Directory {p} is not writable or cannot be created: {e}")
+
 	return p
 
 def parse_args(function_args:dict) -> argparse.Namespace:
@@ -17,6 +43,9 @@ def parse_args(function_args:dict) -> argparse.Namespace:
 	for arg in function_args:
 
 		meta:dict = function_args[arg]
+
+		if not meta.get('CLI'):
+			continue
 
 		long_arg = meta.get('flag')
 		required = meta.get('required')
@@ -28,7 +57,7 @@ def parse_args(function_args:dict) -> argparse.Namespace:
 		parser.add_argument(
 			f"-{arg}",
 			f"--{long_arg}",
-			required=bool(required),
+			required=required is True,
 			help=help,
 			type=type,
 			default=default,
@@ -42,6 +71,7 @@ def merge_commflags_with_kwargs(cli_args:argparse.Namespace=None,function_args:d
 	## Store default values for the function
 	config = {} 
 	for key in function_args.keys():
+
 		if function_args[key].get('default'):
 			config[function_args[key]['flag']] = function_args[key]['default']
 		else:
@@ -50,12 +80,33 @@ def merge_commflags_with_kwargs(cli_args:argparse.Namespace=None,function_args:d
 	## Update with command line arguments
 	if cli_args:
 		config.update(vars(cli_args))
-	
+
 	## Update with function call arguments
 	config.update(kwargs)
 
 	## Check for missing required arguments
-	missing_args = [key for key in function_args.keys() if function_args[key].get('required') and function_args[key]['flag'] not in config]
+	missing_args = []
+
+	for arg in function_args.keys():
+
+		## Skip if argument has been passed
+		if function_args[arg]['flag'] in config:
+			continue
+
+		## Skip if argument is not required
+		if not function_args[arg].get('required'):
+			continue
+
+		## Skip if argument is a CLI argument and CLI args where passed
+		if not function_args[arg]['CLI'] and not cli_args:
+			continue
+
+		## Skip if argument is not a call argument
+		if not function_args[arg]['call']:
+			continue
+
+		missing_args.append(function_args[arg]['flag'])
+
 
 	## Yell about missing args
 	if missing_args:
