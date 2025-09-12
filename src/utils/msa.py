a10e922c31e5eca5bec1d38fd1342c8fd5478380
Alexander <ajulian@hawk.iit.edu>
2025-09-12 10:16:19 -0500

    Rework of MSA module into class structure for memory optimization and ease of use

--- src/utils/msa.py
+++ src/utils/msa.py
@@ -1,288 +1,264 @@
-#!/usr/bin/env python3
-
-from argparse import ArgumentParser
 import os
-from textwrap import wrap
+import time
+import textwrap
 import pandas as pd
 import numpy as np
+import pathlib
+import collections
+import typing
+
 from src.utils.sequence import SequenceUtilities
-from src.utils.general import create_directory
-import time
+from src.utils.general import create_directory, get_file_name
+from src.utils.multiprocess import get_multiprocessing_module
 
-def get_consensus_sequence(msa:pd.DataFrame,outdir:str) -> np.ndarray:
+mp = get_multiprocessing_module()
 
-	outfile = f"{outdir}/consensus.logo"
-	consensus = []
+class MSA:
 
-	with open(outfile,'w') as OUT:
+	char_to_int = {
+		'-':0,
+		'A':1,
+		'B':2,
+		'C':3,
+		'D':4,
+		'E':5,
+		'F':6,
+		'G':7,
+		'H':8,
+		'I':9,
+		'K':10,
+		'L':11,
+		'M':12,
+		'N':13,
+		'P':14,
+		'Q':15,
+		'R':16,
+		'S':17,
+		'T':18,
+		'V':19,
+		'W':20,
+		'Y':21,
+		'X':22,
+		'Z':23
+	}
+
+	int_to_char = {
+		0:'-',
+		1:'A',
+		2:'B',
+		3:'C',
+		4:'D',
+		5:'E',
+		6:'F',
+		7:'G',
+		8:'H',
+		9:'I',
+		10:'K',
+		11:'L',
+		12:'M',
+		13:'N',
+		14:'P',
+		15:'Q',
+		16:'R',
+		17:'S',
+		18:'T',
+		19:'V',
+		20:'W',
+		21:'Y',
+		22:'X',
+		23:'Z'
+	}
+
+	def __init__(self, msa_file:pathlib.Path, outdir:pathlib.Path):
+
+		self.msa_file:pathlib.Path = msa_file
+		self.outdir:pathlib.Path = outdir
+
+		self._load_msa(msa_file=self.msa_file)
+
+		self.sequence_length,self.num_sequences = self.msa.shape
+		self.accessions = self.msa.columns
+
+
+	def _load_msa(self,msa_file:pathlib.Path,outdir:pathlib.Path=pathlib.Path('./')) -> pd.DataFrame:
+
+		"""
+		Reads an MSA from a given file and converts it to a pandas.DataFrame object, where each column is sequence, and each
+		row is the i-th position in the MSA
+
+		Inputs:
+			MSA_File: File path to MSA, either in FASTA or Clustal format.
+
+		Returns:
+			None
+		"""
+
+		msa:dict = {}
+		with open (msa_file, 'r') as MSA_FILE:
+
+			loci = None
+
+			for i, line in enumerate(MSA_FILE):
+
+				line = line.strip()
+				if line == '':
+					continue
 		
-		OUT.write(f"## MSA_position\tRes:Count;\n")
+				split_line = line.split()
 
-		for msa_index,datum in enumerate(np.sum(msa.loc[msa.index,'array'],axis=0).T):
+				if split_line[0][0] == ">":
+					loci = split_line[0][1:].split(".")[0]
+					msa[loci] = []
+					continue
+				elif len(split_line) == 2:
+					loci = split_line[0]
+					line = split_line[1]
 
-			OUT.write(f"{msa_index}\t")
+				msa[loci].append(line.upper())
 
-			for count,residue in sorted(list(zip(datum,SequenceUtilities.AAs)),key=lambda x: x[0],reverse=True):
-				consensus.append(f"{residue}:{int(count)}")
-				OUT.write(f"{consensus[-1]};")
-			OUT.write("\n")
+		msa:dict = {x:"".join(msa[x]) for x in msa.keys()}
 
-	return ";".join(consensus)
+		msa_lengths:list = [len(x) for x in msa.values()]
 
-def read_MSA(msa_file:str=None) -> tuple[dict,str]:
+		if min(msa_lengths) != max(msa_lengths):
+			print(f"\n\t[E]  Aligned sequences do not have the same length (min/max: {min(len(msa_lengths))}/{max(len(msa_lengths))}).")
+			print(f"\n\t[N]  Terminating Two-Entropy calculations.")
+			exit()
 
-	"""Reads an MSA from a given file, and converts it to a dictionary object, with loci and sequences as keys and
-	values, repectively.
+		self.msa = pd.DataFrame({x:[self.char_to_int[y] for y in msa[x]] for x in msa.keys()})
 
-	Inputs:
-		MSA_File: File path to MSA, either in FASTA or Clustal format.
+	def get_reference_indices(self,ref:str) -> np.array:
 
-	Returns:
-		MSA: Dictionary object containig MSAs
-	"""
+		"""
+		Returns the indicies of the MSA where the reference has a defined residue
 
-	file_prefix:str = msa_file.split("/")[-1].split(".")[0]
+		Parameters:
+		ref (str): Accession number of sequence to be used as reference
+		msa (Pandas.DataFrame): Multiple-Sequence Alignment where each row is the j-th aligned protein sequence and each column is the i-th residue of the alignment.
 
-	msa = {}
-	with open (msa_file, 'r') as MSA_FILE:
+		Returns:
+		np.array: Indicies where reference has a defined sequence
+		"""
 
-		loci = None
+		msa = self.msa
 
-		for i, line in enumerate(MSA_FILE):
+		if ref not in msa.columns:
+			print(f"\n\t[W] Reference {ref} is not found in the provided MSA. Skipping to prevent errors\n\n")
+			return None
 
-			line = line.strip()
-			if line == '':
-				continue
-	
-			split_line = line.split()
+		return msa.index[msa[ref]!=0]
 
-			if split_line[0][0] == ">":
-				loci = split_line[0][1:].split(".")[0]
-				msa[loci] = []
-				continue
-			elif len(split_line) == 2:
-				loci = split_line[0]
-				line = split_line[1]
+	def get_residue_counts(self) -> pd.DataFrame:
 
-			msa[loci].append(line.upper())
-
-	msa = {x:"".join(msa[x]) for x in list(msa.keys())}
-
-	return file_prefix,msa
-
-def to_one_liner(aligned_sequences:dict,output:str,reference:str) -> None:
-
-	with open(output,'w') as OUT:
-
-		if reference:
-
-			OUT.write(f">{reference}\n{aligned_sequences[reference]}\n")
-
-		for sequence in sorted(aligned_sequences.keys()):
-
-			if sequence == "reference":
-				next
-
-			OUT.write(f">{sequence}\n{aligned_sequences[sequence]}\n")
-
-	return None
-
-def extract(aligned_sequences:dict,outdir=str) -> None:
-
-	fasta_dir = f"{outdir}/FASTAs" 
-	create_directory(f"{outdir}/FASTAs")
-
-	for locus in aligned_sequences.keys():
-
-		with open(f"{fasta_dir}/{locus}.fasta",'w') as FASTA:
-
-			sequence = "\n".join(wrap(aligned_sequences[locus].replace("-",""),60))
-
-			FASTA.write(f">{locus}\n{sequence}\n")
-
-	return None
-
-def convert(aligned_sequences:dict,align_type:str,outdir:str,file_prefix:str,reference:str) -> None:
-
-	if reference and reference not in aligned_sequences.keys():
-
-		print(f"\n[W]  Reference locus - {reference} - was not found in the alignment set. Defaulting to normal functionality.\n")
-
-		reference = None
-
-	if align_type == "one_liner":
+		"""
+		Returns the counts of all the different residues present in all sequences at the i-th index, for all index in the MSA.
 		
-		to_one_liner(aligned_sequences=aligned_sequences,output=f"{outdir}/{file_prefix}.ola",reference=reference)
-	
-	if align_type == "clustal":
-		...
-	
-	if align_type == "fasta":
-		...
+		Parameters:
+		msa (pandas.DataFrame): Multiple-Sequence Alignment where each row is the j-th aligned protein sequence and each column is the i-th residue of the alignment.
 
-	return
+		Returns:
+		pandas.DataFrame: Sum of residue counts across all sequences for each column (size = AA[24] x MSA_Length)
+		"""
 
-def load_msa(msa_file:str,outdir) -> pd.DataFrame:
+		msa = self.msa
 
-	"""
-	Read in and store MSA, checking whether length is same for all sequences present
-	"""
+		def count_residues(row) -> dict:
 
-	print(f"\n\t\tLoading alignment data from {msa_file}")
+			counts = collections.Counter([item for item in row if item in self.int_to_char.keys()])
 
-	AAs = SequenceUtilities.AAs
+			return {numeric: counts.get(numeric, 0) for numeric in self.int_to_char.keys()}
 
-	data_dir = f"{outdir}/.data"
-	pickle_file = f"{data_dir}/MSA.pkl"
-	create_directory(data_dir)
+		## Get residue counts 
+		msa_residue_counts = msa.apply(count_residues,axis=1,result_type='expand')
 
-	if os.path.isfile(pickle_file):
-		print(f"\n\t\t\t[N] Found previously processed MSA data")
-		print(f"\n\t\t\t[N] Loading previously processed MSA data")
-		start = time.time()
-		arrayed_MSA = pd.read_pickle(pickle_file,compression='gzip')
-		print(f"\n\t\t\tPreviously processed MSA data loaded in {time.time()-start:.0f}s")
-		return arrayed_MSA
+		msa_residue_counts =  pd.DataFrame((msa_residue_counts)).fillna(0).astype(int)
 
-	print(f"\n\t\tReading alignment data from MSA file {msa_file}")
+		return msa_residue_counts
 
-	start = time.time()
-	msa = read_MSA(msa_file=msa_file)
+	def _calculate_distances(self,sequence_a_loc:str) -> typing.Tuple[str,dict]:
 
-	print(f"\n\t\t\tAlignment data loaded in {time.time()-start:.0f}s")
+		sequence_a = self.msa[sequence_a_loc]
 
-	msa_keys = [x for x in msa.keys()]
+		distances = {x:0 for x in self.accessions}
 
-	msa = {x:[i for i in msa[x]] for x in msa_keys}
+		for sequence_b_loc in sorted(distances.keys()):
+			if sequence_b_loc == sequence_a_loc:
+				distances[sequence_b_loc] = np.inf
+				return sequence_a_loc,distances
+			distances[sequence_b_loc] = np.sum(sequence_a!=self.msa[sequence_b_loc])/2
 
-	len_max = max([len(msa[x]) for x in msa_keys])
-	len_min = min([len(msa[x]) for x in msa_keys])
+		return sequence_a_loc,distances
 
-	if len_min != len_max:
-		print(f"\n\t[E]  Aligned sequences do not have the same length (min/max: {len_min}/{len_max}).")
-		print(f"\n\t[N]  Terminating Two-Entropy calculations.")
-		exit()
+	def calculate_sequence_difference(self,threads:int=1) -> pd.DataFrame:
 
-	pause = time.time()
+		"""Calculates the distances between sequences in an MSA
 
-	print(f"\n\t\tProcessing alignment data")
+		Returns:
+		"""
 
-	## Create a database where each sequence has a flattened matrix that is the size of the alignment length x the number of recognized amino acids
-	arrayed_MSA = pd.DataFrame({'array':np.array(np.zeros((len(AAs),len_max)) for key in msa_keys)},dtype=object,index=msa_keys)
+		keys:list = sorted(self.accessions)
+		self.distance_matrix:pd.DataFrame = pd.DataFrame(np.zeros([len(keys),len(keys)]),columns=keys,index=keys)
 
-	## Iterate over every sequence
-	for key in msa_keys:
 
-		## Iterate through the entire sequence
-		for index,res in enumerate(msa[key]):
+		results:list
+		with mp.Pool(threads) as executor:
+			results = executor.map(self._calculate_distances,keys)
+
+		for sequence_loc_a,result in results:
+			self.distance_matrix.loc[sequence_loc_a] = result
+
+		self.distance_matrix += self.distance_matrix.T
+
+		return self.distance_matrix
+
+	# def to_one_liner(aligned_sequences:dict,output:str,reference:str) -> None:
+
+	# 	with open(output,'w') as OUT:
+
+	# 		if reference:
+
+	# 			OUT.write(f">{reference}\n{aligned_sequences[reference]}\n")
+
+	# 		for sequence in sorted(aligned_sequences.keys()):
+
+	# 			if sequence == "reference":
+	# 				next
+
+	# 			OUT.write(f">{sequence}\n{aligned_sequences[sequence]}\n")
+
+	# 	return None
+
+	# def extract(aligned_sequences:dict,outdir=str) -> None:
+
+	# 	fasta_dir = f"{outdir}/FASTAs" 
+	# 	create_directory(f"{outdir}/FASTAs")
+
+	# 	for locus in aligned_sequences.keys():
+
+	# 		with open(f"{fasta_dir}/{locus}.fasta",'w') as FASTA:
+
+	# 			sequence = "\n".join(textwrap.wrap(aligned_sequences[locus].replace("-",""),60))
+
+	# 			FASTA.write(f">{locus}\n{sequence}\n")
+
+	# 	return None
+
+	# def convert(self,aligned_sequences:dict,align_type:str,outdir:str,file_prefix:str,reference:str) -> None:
+
+	# 	if reference and reference not in aligned_sequences.keys():
+
+	# 		print(f"\n[W]  Reference locus - {reference} - was not found in the alignment set. Defaulting to normal functionality.\n")
+
+	# 		reference = None
+
+	# 	if align_type == "one_liner":
 			
-			## Assign the residue at the current position a value of 1
-			arrayed_MSA.loc[key,'array'][AAs.index(res)][index] = 1
+	# 		self.to_one_liner(aligned_sequences=aligned_sequences,output=f"{outdir}/{file_prefix}.ola",reference=reference)
+		
+	# 	if align_type == "clustal":
+	# 		...
+		
+	# 	if align_type == "fasta":
+	# 		...
 
-	# arrayed_MSA.to_pickle(pickle_file,compression='gzip')
-
-	print(f"\n\t\t\tAlignment data prcocessed in {time.time()-pause:.0f}s")
-
-	return arrayed_MSA
-
-def read_MSA(msa_file:str=None) -> dict:
-
-	"""Reads an MSA from a given file, and converts it to a dictionary object, with loci and sequences as keys and
-	values, repectively.
-
-	Inputs:
-		MSA_File: File path to MSA, either in FASTA or Clustal format.
-
-	Returns:
-		MSA: Dictionary object containig MSAs
-	"""
-
-	msa = {}
-	with open (msa_file, 'r') as MSA_FILE:
-
-		loci = None
-
-		for i, line in enumerate(MSA_FILE):
-
-			line = line.strip()
-			if line == '':
-				continue
-	
-			split_line = line.split()
-
-			if split_line[0][0] == ">":
-				loci = split_line[0][1:].split(".")[0]
-				msa[loci] = []
-				continue
-			elif len(split_line) == 2:
-				loci = split_line[0]
-				line = split_line[1]
-
-			msa[loci].append(line.upper())
-
-	return {x:"".join(msa[x]) for x in list(msa.keys())}
-
-def get_reference_indices(ref:str,msa:pd.DataFrame) -> np.array:
-
-	"""
-	Returns the indicies of the MSA where the reference has a defined residue
-
-	Parameters:
-	ref (str): Accession number of sequence to be used as reference
-	msa (Pandas.DataFrame): Multiple-Sequence Alignment where each row is the j-th aligned protein sequence and each column is the i-th residue of the alignment.
-
-	Returns:
-	np.array: Indicies where reference has a defined sequence
-	"""
-
-	if ref not in msa.index:
-		print(f"\n\t[W] Reference {ref} is not found in the provided MSA. Skipping to prevent errors\n\n")
-		return None
-
-	return np.where((msa.loc[ref,'array'].T != 1)[:,-1])[0]
-
-def get_residue_counts(msa:pd.DataFrame) -> np.ndarray:
-
-	"""
-	Returns the counts of all the different residues present in all sequences at the i-th index, for all index in the MSA.
-	 
-	Parameters:
-	msa (Pandas.DataFrame): Multiple-Sequence Alignment where each row is the j-th aligned protein sequence and each column is the i-th residue of the alignment.
-
-	Returns:
-	np.ndarray: Sum of residue counts across all sequences for each column (size = AA[24] x MSA_Length)
-	"""
-	
-	return np.sum(msa['array'],axis=0)
-
-def run(msa_file:str,outdir:str,extract_fastas:str,desired_alignment_type:str,reference:str) -> int:
-
-	create_directory(outdir)
-
-	file_prefix:str
-	alignment_data:dict
-
-	file_prefix,alignment_data = read_MSA(msa_file=msa_file)
-
-	convert( aligned_sequences=alignment_data,
-			 align_type=desired_alignment_type,
-			 outdir=outdir,
-			 file_prefix=file_prefix,
-			 reference=reference
-	)
-
-	if extract_fastas: extract(aligned_sequences=alignment_data,outdir=outdir)
-
-	return 0
-
-if __name__ == "__main__":
-
-	parser = ArgumentParser()
-
-	parser.add_argument("-m","--msa_file",required=True,type=str)
-	parser.add_argument("-o","--outdir",default="alignment_conversion")
-	parser.add_argument("-x","--extract_fastas",default=False,action='store_true')
-	parser.add_argument("-a","--desired_alignment_type",default=["one_liner"],choices=["one_liner,clustal,fasta"])
-	parser.add_argument("-r","--reference",default=None,type=str)
-
-	run(parser.parse_known_args()[0])
+	# 	return
