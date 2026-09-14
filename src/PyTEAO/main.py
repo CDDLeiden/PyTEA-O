af9d9e5719471ead2fa6128ff01b7709fc485c59
Alexander Thomas Julian <ajulian@hawk.illinoistech.edu>
2026-09-13 21:36:48 -0500

    Switched over to click and fixed missing reference id pass to MSA

--- src/PyTEAO/main.py
+++ src/PyTEAO/main.py
@@ -1,115 +1,144 @@
 #!/usr/bin/env python3
 
-import argparse
+import click
+import typing
+import pathlib
 
 from PyTEAO import general, validation
 
-ARGUEMENTS = {
-	'm':{
-		'flag':'msa_file',
-		'required': False,
-		'help':"File path to Multiple Sequence Alignment",
-		'type':general.valid_file,
-	},
-	'o': {
-		'flag': 'outdir',
-		'help': "Output file location",
-		'type': str,
-		'default': "PyTEA-O_Results",
-		'type': general.valid_directory,
-	},
-	'r': {
-		'flag': 'reference_id',
-		'help': "Reference ID",
-		'type': str,
-	},
-	't': {
-		'flag': 'threads',
-		'help': "Number of threads",
-		'type': int,
-		'default': 1,
-	},
-	's': {
-		'flag': 'subfamilies_file',
-		'help': "File location of subfamily assignment (TXT file)",
-		'type': general.valid_file,
-	},
-	'g': {
-		'flag': 'highlight_file',
-		'help': "File location for residues to highlight",
-		'type': general.valid_file,
-	},
-	'l': {
-		'flag': 'plot_layout',
-		'help': "Command line override for modifying subplots produced in figure",
-		'type': general.binary,
-		'default': '111111'
-	},
-	'b': {
-		'flag': 'tree_type',
-		'help': "Method to group sequences provided in the MSA",
-		'type': str,
-		'default': 'distance_matrix',
-		'choices':['distance_matrix','lineage']
-	},
-	'v': {
-		'flag': 'validate',
-		'help': "Run TEA-O validation process",
-		'action': 'store_true',
-		'default': None
-	}
-}
+@click.command()
+@click.option(
+	"-m","--msa-file",
+	help="File containing aligned sequences",
+	type=click.Path(path_type=pathlib.Path,dir_okay=False,readable=True,exists=True),
+	required=True
+)
+@click.option(
+	"-o","--outdir",
+	help="Output directory to place results",
+	type=click.Path(path_type=pathlib.Path,file_okay=False,readable=True,writable=True,exists=False),
+	default="PyTEA-O",
+	required=False
+)
+@click.option(
+	"-r","--reference-id",
+	help="Accession number of sequence in MSA to use as base in figure generation",
+	type=str,
+	default=None
+)
+@click.option(
+	"-t","--threads",
+	help="Number of threads allowed for process use",
+	type=int,
+	default=1
+)
+@click.option(
+	"-s","--subfamilies-file",
+	help="File containing subfamily assignments",
+	type=click.Path(path_type=pathlib.Path,dir_okay=False,exists=True,readable=True),
+	required=False
+)
+@click.option(
+	"-g","--highlight-file",
+	help="File containing residue-specific highlights",
+	type=click.Path(path_type=pathlib.Path,dir_okay=False,exists=True,readable=True),
+	required=False
+)
+@click.option(
+	"-l","--plot-layout",
+	help="Command line override for modifying subplots present in output figure",
+	type=general.binary,
+	default='111111',
+	required=False
+)
+@click.option(
+	"-b","--tree-type",
+	help="Method for grouping sequences in the provided MSA",
+	type=click.Choice(['dist','lin']),
+	default='dist',
+	required=False
+)
+@click.option(
+	"-v","--validate",
+	help="Run TEA-O validation analysis",
+	type=bool,
+	is_flag=True,
+	default=False,
+	required=False
+)
+def main(
+		msa_file:pathlib.Path,
+		*,
+		outdir:pathlib.Path=pathlib.Path("./PyTEA-O"),
+		reference_id:str|None=None,
+		threads:int=1,
+		subfamilies_file:pathlib.Path|None=None,
+		highlight_file:pathlib.Path|None=None,
+		plot_layout:general.binary='111111',
+		tree_type:typing.Literal['dist','lin'] = 'dist',
+		validate:bool=False
+	) -> None:
 
-def main(args:argparse.Namespace|None=None) -> None:
+	(outdir := pathlib.Path(outdir)).mkdir(mode=0o755,parents=True,exist_ok=True)
 
-	## Create and parse arguments
-	args:argparse.Namespace = general.parse_args(
-		function_args=ARGUEMENTS,
-	)
+	if not validate and msa_file is None:
+		raise ValueError(f"The --msa_file argument is required unless --validate is used.")
 
-	## Validate Ye's Results
-	if args.validate:
 
-		validation.validate(args)
-
+	## Validate pipeline against Ye's Results
+	if validate:
+		validation.validate(outdir)
 		return
 	
-	if not args.validate and args.msa_file is None:
-		raise ValueError(f"The --msa_file argument is required unless --validate is used.")
 
 	from PyTEAO import MSA
 
 	## Load the MSA
-	msa = MSA(args.msa_file,args.outdir,threads=args.threads)
+	msa = MSA(
+			msa_file,
+			outdir,
+			threads=threads,
+			reference_accession=reference_id
+		)
 
 
 	from PyTEAO import PhyloTree, TaxonTree, UserTree, Tree
 
 	## Build Tree for Two Entropy Calculations
 	tree:Tree
-	if args.subfamilies_file is not None:
-		tree = UserTree(msa,args.subfamilies_file)
-	elif args.tree_type == 'lineage':
+	if subfamilies_file is not None:
+		tree = UserTree(msa,subfamilies_file)
+	elif tree_type == 'lin':
 		tree = TaxonTree(msa)
-	elif args.tree_type == 'distance_matrix':
+	elif tree_type == 'dist':
 		tree = PhyloTree(msa)
 	else:
-		raise ValueError(f"Invalid tree_type {args.tree_type} provided, 'lineage' or 'distance_matrix' supported.")
+		raise ValueError(f"Invalid tree_type {tree_type} provided, 'lineage' or 'distance_matrix' supported.")
 
 	from PyTEAO import TwoEntropyAnalysis as TEA
 
 	## Perform Two Entropy Analaysis
-	tea = TEA(msa,tree,threads=args.threads,outdir=args.outdir)
-
+	tea = TEA(
+			msa,
+			tree,
+			threads=threads,
+			outdir=outdir,
+		)
 
 	from PyTEAO import PlotManager
 
 	## Plot data
-	figure = PlotManager(tea=tea,subplots=args.plot_layout,outdir=args.outdir,highlight_file=args.highlight_file)
+	figure = PlotManager(
+			tea=tea,
+			subplots=plot_layout,
+			outdir=outdir,
+			highlight_file=highlight_file
+		)
 
 	figure.save_fig(file_type="png")
+	figure.save_fig(file_type="svg")
 
 
 if __name__ == "__main__":
 
-	main(args=general.parse_args(function_args=ARGUEMENTS))
+	main()
